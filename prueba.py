d2a970fc5704aa3ab15d48ba5d929a77de84f9b3
J.Crhistian <j.crhistiantr@gmail.com>
2026-03-23 18:08:38 -0400

    Creacion de la primera prueba con el script

--- prueba.py
+++ prueba.py
@@ -1,3 +1,7 @@
 from alpaca.trading.client import TradingClient
 
-client = TradingClient("")
+client = TradingClient("PKGDJHPBXIO3QTVYDDZO4JRBQE","GvqQ44ocqYj1Kuivf4e9CiRek7s5E728x1vQtckZiTuz")
+
+print(client.get_account().buying_power)
+print(client.get_account().cash)
+print(client.get_account().account_number)
